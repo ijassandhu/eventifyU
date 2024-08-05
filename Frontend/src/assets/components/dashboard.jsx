4a55532ef95a85ef646ca12bc9579e58228e2fbb
Jaskeerat <jaskeeratsingh219@gmail.com>
2024-08-05 23:39:30 +0530

    Dashboard

--- Frontend/src/assets/components/dashboard.jsx
+++ Frontend/src/assets/components/dashboard.jsx
@@ -5,42 +5,100 @@ import styles from './css/clubs.module.css';
 const Dashboard = () => {
   return (
     <>
-      <div className='bg-[#2b2b2b] flex justify-center w-full h-64'>
-        <div className='pt-16 flex flex-col md:flex-row mr-2'>
-          <div className="inputParent wrap-input-2 p-2 md:p-10">
-            <label htmlFor="name" className="text-white">Name:</label>
+      <div className='bg-[#2b2b2b] flex justify-center w-full px-16'>
+        <div className='pt-24 flex flex-col md:flex-row flex-wrap justify-evenly'>
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label htmlFor="name" className="text-white self-start md:text-lg">Name:</label>
             <input
               id="name"
-              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
               type="text"
-              placeholder="Name"
+              required
             />
-            <span className="focus-border"></span>
           </div>
-          <div className="inputParent wrap-input-2 p-2 md:p-10">
-            <label className="text-white">Email:</label>
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label className="text-white self-start md:text-lg">Email:</label>
             <input
               id="email"
-              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
               type="text"
-              placeholder="Email"
-            />
+              required />
           </div>
-          <div className="inputParent wrap-input-2 p-2 md:p-10">
-            <label className="text-white">Contact:</label>
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label className="text-white self-start md:text-lg">Contact:</label>
             <input
               id="contact"
-              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
               type="number"
-              placeholder="Contact"
+              required
+            />
+          </div>
+
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label className="text-white self-start md:text-lg">Course:</label>
+            <input
+              id="contact"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
+              type="text"
+              required
+            />
+          </div>
+
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label className="text-white self-start md:text-lg">Department:</label>
+            <input
+              id="contact"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
+              type="text"
+              required
+            />
+          </div>
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label className="text-white self-start md:text-lg">Semester:</label>
+            <input
+              id="contact"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
+              type="text"
+              required
+            />
+          </div>
+
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label className="text-white self-start md:text-lg">College:</label>
+            <input
+              id="contact"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
+              type="text"
+              required
+            />
+          </div>
+
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label className="text-white self-start md:text-lg">Roll Number:</label>
+            <input
+              // defaultValue={value.rollNo}
+              id="contact"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
+              type="number"
+              required
+            />
+          </div>
+          <div className="inputParent wrap-input-2 p-2 md:p-3 flex flex-col">
+            <label className="text-white self-start md:text-lg">Section:</label>
+            <input
+              id="contact"
+              className="input pl-3 h-10 w-[80vw] md:w-[25vw] rounded-xl bg-[#2b2b2b] border border-white text-white"
+              type="number"
+              required
             />
           </div>
         </div>
       </div>
 
-        <div className='md:hidden'>
-          <Footericons/>
-        </div>
+
+      <div className='md:hidden'>
+        <Footericons />
+      </div>
     </>
   );
 
