b6bda283e86cc79a7eca2e4a5de2e8f256d37b73
Jaskeerat <jaskeeratsingh219@gmail.com>
2024-08-11 00:36:52 +0530

    fixed some issues

--- Frontend/src/assets/components/navbar.jsx
+++ Frontend/src/assets/components/navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Search } from 'lucide-react';
 import Login from "./Login.jsx";
 import Signup from "./signup.jsx";
 import dummy from '/photos/dummy.png';
@@ -22,12 +23,12 @@ const Navbar = () => {
 
     return (
         <div>
-            <nav className="flex bg-[#222] md:h-20 h-16 items-center fixed top-0 w-[100vw] z-10 justify-between md:justify-around px-4 md:px-0 shadow-lg shadow-[#99EA73]/30">
-                <div className="text-[#99EA73] text-2xl flex">
+            <nav className="flex bg-[#222] md:h-20 h-16 items-center fixed top-0 w-[100vw] z-10 px-4 md:px-0 shadow-lg shadow-[#99EA73]/30">
+                <div className="text-[#99EA73] text-2xl flex absolute left-20">
                     <h1 className="hover:text-white transition-colors duration-300">event</h1>
                     <h1 className="text-white hover:text-[#99EA73] transition-colors duration-300">ifyU</h1>
                 </div>
-                <ul className="md:flex md:flex-row space-x-14 md:space-x-9 text-lg sm:text-md text-white hidden">
+                <ul className="md:flex md:flex-row space-x-14 md:space-x-9 text-lg sm:text-md text-white hidden absolute left-2/4 -translate-x-2/4">
                     {['/', '/events', '/dashboard', '/clubs', '/feedback'].map((path, index) => (
                         <li key={index} className={`hover:text-[#99EA73] transition-colors duration-300 ${location.pathname === path ? 'text-[#99EA73]' : ''}`}>
                             <Link to={path}>{path === '/' ? 'Home' : path.slice(1).charAt(0).toUpperCase() + path.slice(2)}</Link>
@@ -52,31 +53,34 @@ const Navbar = () => {
                         {showModal2 && <Signup onClose={() => setShowModal2(false)} toggleModal={toggleModal} />}
                     </div>
                 ) : (
-                    <div className="flex items-center gap-4">
-                        <h1 className="hidden md:block text-white text-sm md:text-xl">Hello! {user.name}</h1>
-                        <div className="relative group">
+
+                    <div className="flex items-center absolute right-20">
+                        {/* <h1 className="hidden md:block text-white text-sm md:text-xl">Hello! {user.name}</h1> */}
+                        <div>
+                            {location.pathname === '/clubs' || location.pathname === '/events' ? (
+                                <>
+                                    <div className="flex">
+                                        <input
+                                            type="text"
+                                            placeholder="Search..."
+                                            className="p-2 outline-none w-28 md:w-40 rounded-l-md h-8 bg-[#fff] text-black text-sm"
+                                        />
+                                        <div className="text-black border bg-white h-8 w-8 text-sm rounded-r-md flex items-center justify-center">
+                                            <Search />
+                                        </div>
+                                    </div>
+                                </>
+                            ) : (
+                                ''
+                            )}
+                        </div>
+
+                        <div className="relative group pl-4">
                             <img
                                 src={dummy}
                                 alt="User Avatar"
                                 className="w-10 h-10 rounded-full cursor-pointer border-2 border-[#99EA73] transition-transform duration-300 transform group-hover:scale-110"
                             />
-                            {/* <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg overflow-hidden opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-300 ease-in-out">
-                                <Link to="/profile" className="block px-4 py-2 text-gray-800 hover:bg-[#99EA73]/20 hover:text-[#222] transition-all duration-300">
-                                    Profile
-                                </Link>
-                                <Link to="/my-events" className="block px-4 py-2 text-gray-800 hover:bg-[#99EA73]/20 hover:text-[#222] transition-all duration-300">
-                                    My Events
-                                </Link>
-                                <Link to="/settings" className="block px-4 py-2 text-gray-800 hover:bg-[#99EA73]/20 hover:text-[#222] transition-all duration-300">
-                                    Settings
-                                </Link>
-                                <button
-                                    onClick={() => setLoggedIn(false)}
-                                    className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-[#99EA73]/20 hover:text-[#222] transition-all duration-300"
-                                >
-                                    Log Out
-                                </button>
-                            </div> */}
                         </div>
                     </div>
                 )}
