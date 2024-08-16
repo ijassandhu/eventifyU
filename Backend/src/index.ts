02fa1399e58b03782cc9f0f73c521c4777f552a8
Jashanveer Singh <puniajashan85@gmail.com>
2024-08-16 16:30:01 +0530

    some changes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Jor se Hillao Typescript")
+import express, {Request, Response} from "express";
+import bodyParser from "body-parser";
+import cors from 'cors';
+// import dotenv from 'dotenv';
+
+const app = express();
+
+// dotenv.config({path: './env'})
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (_req: Request, res:Response) => {
+  res.json({
+    success: true,
+    message: "working",
+  });
+});
+
+app.listen(5000, () => {
+  console.log(`Running server at localhost:5000`);
+});
