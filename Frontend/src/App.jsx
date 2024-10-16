84b88b48fa43134290b4acf0f5e2b4886fe2b761
Jashanveer Singh <puniajashan85@gmail.com>
2024-10-16 23:35:01 +0530

    user and club working

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Events from "./components/events.jsx";
 import Clubs from "./components/clubs.jsx";
 import Feedback from "./components/feedback.jsx";
 import Dashboard from "./components/dashboard.jsx";
-import NotFound from "./components/Notfound.jsx";
+import NotFound from "./components/NotFound.jsx";
 import ClubTemplate from "./components/clubs/clubtemplate.jsx";
 import Contact from "./components/contactus.jsx";
 import FAQ from "./components/faq.jsx";
